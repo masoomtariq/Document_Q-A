c8603164f0de585d42193b589b24df395aec11c4
Masoom Tariq <167184654+masoomtariq@users.noreply.github.com>
2025-02-11 15:48:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def generate_responce():
     retreival = get_queries | st.session_state.vectors.as_retriever().map() | get_unique
     template = """
     You are a helpful and informative document question-answering assistant.  Your primary goal is to provide accurate and insightful answers based *exclusively* on the provided context.  You are an expert at synthesizing information and drawing connections within the given text.  Do not rely on any external knowledge or information beyond what is explicitly given in the context.
-    If the user say HII or anything else like that then greet the user politely and show the user how you can help with the provided document
+    If the user say HII or something like that then greet the user politely and show the user how you can help with the provided document
     If the user expresses gratitude or indicates the end of the conversation, respond with a polite farewell.
     Always maintain a polite and professional tone.
 **Instructions for Enhancing Context-Based Responses:**
